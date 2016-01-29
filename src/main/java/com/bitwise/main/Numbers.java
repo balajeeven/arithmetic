44ac3f7186a1a996f68559b890058422419348dd
Balajee Venkatesh <balajee.venkatesh@bitwiseglobal.com>
2016-01-29 17:57:45 +0530

    Update Numbers.java

--- src/main/java/com/bitwise/main/Numbers.java
+++ src/main/java/com/bitwise/main/Numbers.java
@@ -1,11 +1,3 @@
 package com.bitwise.main;
 
 /**
- * Created by akankshag on 1/28/2016.
- */
-public class Numbers {
-    int number;
-    public Numbers(int number) {
-        this.number=number;
-    }
-}
